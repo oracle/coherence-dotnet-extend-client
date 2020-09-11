b8043a8b56493cc16987f906b0a9ccda9b33a798
Chinmay Patel <chinmay.patel@oracle.com>
2020-09-11 17:16:19 +0000

    Bug 31877828 - [31819882->14.1.1.0.2] RFA: [NET] Client Library cache-config local-certificates selector element not processed.
    [git-p4: depot-paths = "//dev/release.net/coherence-net-v14.1.1.0/": change = 81634]

---
+++ tests/Coherence.Tests/Net/Ssl/ClientCertificateSelector.cs
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2000, 2020, Oracle and/or its affiliates.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at
+ * http://oss.oracle.com/licenses/upl.
+ */
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Tangosol.Net.Ssl
+{
+    public class ClientCertificateSelector
+    {
+
+        public ClientCertificateSelector()
+        {
+        }
+
+        public static X509Certificate SelectClientCertificate(
+                    object sender,
+                    string targetHost,
+                    X509CertificateCollection localCertificates,
+                    X509Certificate remoteCertificate,
+                    string[] acceptableIssuers)
+        {
+            Console.WriteLine(typeof(ClientCertificateSelector).Name +
+                    ": Using custom delegate method for client certificate selector.");
+
+            if (localCertificates != null && localCertificates.Count > 0)
+            {
+                return localCertificates[0];
+            }
+
+            throw new InvalidOperationException("Expected one or more client certificates.");
+        }
+    }
+}
