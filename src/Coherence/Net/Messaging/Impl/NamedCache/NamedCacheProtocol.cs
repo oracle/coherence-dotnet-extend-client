6fb5e80764130d1e22bd8ebb7c99a97e234e2855
luk.ho <luk.ho@oracle.com>
2022-05-05 12:08:41 +0000

    Enh 34136392 - [33867014->14.1.2.0.0] .NET: Port [ENH 33796444 - CacheEvent should show whether a delete was due to expiry] to .NET
    [git-p4: depot-paths = "//dev/main.net/": change = 92263]

--- src/Coherence/Net/Messaging/Impl/NamedCache/NamedCacheProtocol.cs
+++ src/Coherence/Net/Messaging/Impl/NamedCache/NamedCacheProtocol.cs
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2020, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2022, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
@@ -49,7 +49,7 @@ namespace Tangosol.Net.Messaging.Impl.NamedCache
         /// </summary>
         protected NamedCacheProtocol()
         {
-            CurrentVersion   = 7;
+            CurrentVersion   = 9;
             SupportedVersion = 2;
         }
 
