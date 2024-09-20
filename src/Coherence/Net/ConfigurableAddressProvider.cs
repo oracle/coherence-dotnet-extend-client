8fc8593a0d85b8beb66b42d4f8f17eb44141c62c
Vaso Putica <vaso.putica@oracle.com>
2024-09-20 05:43:56 -0500

    COH-30785 Cleanup build warnings in .NET client - additional cleanup
    [git-p4: depot-paths = "//dev/main.net/": change = 111499]

--- src/Coherence/Net/ConfigurableAddressProvider.cs
+++ src/Coherence/Net/ConfigurableAddressProvider.cs
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 2000, 2021, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2024, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
- * http://oss.oracle.com/licenses/upl.
+ * https://oss.oracle.com/licenses/upl.
  */
 using System;
 using System.Collections.Generic;
@@ -306,7 +306,7 @@ namespace Tangosol.Net
                 }
                 else
                 {
-                    throw e;
+                    throw;
                 }
             }
 
--- tests/Coherence.Tests/Net/Ssl/SslTests.cs
+++ tests/Coherence.Tests/Net/Ssl/SslTests.cs
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 2000, 2020, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2024, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
- * http://oss.oracle.com/licenses/upl.
+ * https://oss.oracle.com/licenses/upl.
  */
 using System;
 using System.IO;
@@ -80,7 +80,7 @@ namespace Tangosol.Net.Ssl
                 client.Echo("Hello World");
                 Assert.Fail("Expected IOException, but got none.");
             }
-            catch (IOException e)
+            catch (IOException)
             {}
             finally
             {
