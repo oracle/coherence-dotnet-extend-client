5bb3c56f1905644cc312161a27720439fd36b65f
Vaso Putica <vaso.putica@oracle.com>
2024-09-25 09:46:06 -0500

    COH-30977 .NET log messages include the cl number and "(TEST)"
    [git-p4: depot-paths = "//dev/main.net/": change = 111572]

--- src/Coherence/Util/Logging/Logger.cs
+++ src/Coherence/Util/Logging/Logger.cs
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 2000, 2020, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2024, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
- * http://oss.oracle.com/licenses/upl.
+ * https://oss.oracle.com/licenses/upl.
  */
 using System;
 using System.Reflection;
@@ -99,7 +99,7 @@ namespace Tangosol.Util.Logging
         /// </value>
         public static string DefaultFormat
         {
-            get { return "{date} {product} {version} &lt;{level}&gt; (thread={thread}): {text}"; }
+            get { return "{date} {product} {version} <{level}> (thread={thread}): {text}"; }
         }
 
         /// <summary>
