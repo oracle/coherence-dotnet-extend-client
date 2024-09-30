73821250980923f891c80f87cb1483a22f442f70
Vaso Putica <vaso.putica@oracle.com>
2024-09-30 11:38:00 -0500

    COH-30975 .NET main Linux and macOS builds are failing on GitHub
    [git-p4: depot-paths = "//dev/main.net/": change = 111679]

--- tests/Coherence.Tests/Net/Impl/SSLRemoteNamedCacheTests.cs
+++ tests/Coherence.Tests/Net/Impl/SSLRemoteNamedCacheTests.cs
@@ -1,23 +1,20 @@
 /*
- * Copyright (c) 2000, 2020, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2024, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
- * http://oss.oracle.com/licenses/upl.
+ * https://oss.oracle.com/licenses/upl.
  */
 using System;
 using System.IO;
-using System.Collections;
 using System.Collections.Specialized;
-using System.Threading;
 
 using NUnit.Framework;
-using Tangosol.Net.Cache;
-using Tangosol.Net.Messaging.Impl.NamedCache;
-using Tangosol.Util;
+using Tangosol.Run.Xml;
 
 namespace Tangosol.Net.Impl {
 
     [TestFixture]
+    [Platform(Exclude="Unix,Linux,MacOsX")]
     public class SSLRemoteNamedCacheTests
     {
 
@@ -33,6 +30,20 @@ namespace Tangosol.Net.Impl {
         {
             get { return appSettings.Get("sslTwoWayCacheName"); }
             set { m_cacheName = value; }
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            var ccf    = CacheFactory.ConfigurableCacheFactory;
+            var config = XmlHelper.LoadXml("assembly://Coherence.Tests/Tangosol.Resources/s4hc-cache-config-ssl.xml");
+            ccf.Config = config;
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            CacheFactory.Shutdown();
         }
 
         [Test]
--- tests/Coherence.Tests/Net/Impl/SSLTwoWayRemoteNamedCacheTests.cs
+++ tests/Coherence.Tests/Net/Impl/SSLTwoWayRemoteNamedCacheTests.cs
@@ -1,25 +1,37 @@
 /*
- * Copyright (c) 2000, 2020, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2024, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
- * http://oss.oracle.com/licenses/upl.
+ * https://oss.oracle.com/licenses/upl.
  */
 using System;
-using System.IO;
 
 using NUnit.Framework;
-using Tangosol.Net.Cache;
-using Tangosol.Net.Messaging.Impl.NamedCache;
-using Tangosol.Util;
+using Tangosol.Run.Xml;
 
 namespace Tangosol.Net.Impl
 {
     [TestFixture]
+    [Platform(Exclude="Unix,Linux,MacOsX")]
     public class SSLTwoWayRemoteNamedCacheTests : RemoteNamedCacheTests
     {
         protected override String TestCacheName
         {
             get { return "sslTwoWayCacheName"; }
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            var ccf    = CacheFactory.ConfigurableCacheFactory;
+            var config = XmlHelper.LoadXml("assembly://Coherence.Tests/Tangosol.Resources/s4hc-cache-config-ssl.xml");
+            ccf.Config = config;
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            CacheFactory.Shutdown();
         }
 
         [Test]
